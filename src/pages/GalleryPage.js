5e0f20672f437c8cb4dec4a8b272dcff09ce3654
Julia von Goetz <jvongoetz808@gmail.com>
2024-02-13 13:40:47 -0800

    gallery and about are formatted, more css needed

--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -1,10 +1,50 @@
 import React from 'react';
+import '../App.css';
+import matt from '../photos/matt.png';
 
 const GalleryPage = () => {
   return (
-    <div>
-      {/* Add your layout and components here */}
-      <h1>Gallery Page</h1>
+    <div className="gallery-page">
+        <h1>Gallery</h1>
+        <div className="images-container">
+          
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+          <div className="image">
+            <img src={matt} alt="Club Image" />
+          </div>
+
+        </div>
     </div>
   );
 };
