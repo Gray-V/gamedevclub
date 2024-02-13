aa8a5aa19e91fce24ddd556204a47fac01ec0e21
Julia von Goetz <jvongoetz808@gmail.com>
2024-02-12 19:20:14 -0800

    eboard

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,12 @@
 // src/pages/AboutPage.js
 import React from 'react';
-import '../App.css'; // Make sure to create an AboutPage.css file
+import '../App.css';
+import gray from '../photos/gray.png';
+import matt from '../photos/matt.png';
+import ryan from '../photos/ryan.png';
+import jake from '../photos/jake.png';
+import ary from '../photos/ary.png';
+import jer from '../photos/jer.png';
 
 const AboutPage = () => {
   return (
@@ -12,37 +18,37 @@ const AboutPage = () => {
         <h2>E-Board Members</h2>
         <div className="eboard-members">
           <div className="eboard-member">
-            <img src="/path-to-image.jpg" alt="E-Board Member" />
+            <img src={gray} alt="E-Board Member" />
             <p>President</p>
             <p>Grayson von Goetz</p>
           </div>
 
           <div className="eboard-member">
-            <img src="/path-to-image.jpg" alt="E-Board Member" />
+            <img src={matt} alt="E-Board Member" />
             <p>Vice President</p>
             <p>Matthew Lee</p>
           </div>
 
           <div className="eboard-member">
-            <img src="/path-to-image.jpg" alt="E-Board Member" />
+            <img src={ryan} alt="E-Board Member" />
             <p>Treasurer</p>
             <p>Ryan Gilbert</p>
           </div>
 
           <div className="eboard-member">
-            <img src="/path-to-image.jpg" alt="E-Board Member" />
+            <img src={jake} alt="E-Board Member" />
             <p>Secretary</p>
             <p>Jacob Martinez</p>
           </div>
 
           <div className="eboard-member">
-            <img src="/path-to-image.jpg" alt="E-Board Member" />
+            <img src={ary} alt="E-Board Member" />
             <p>Community Manager</p>
             <p>Aryman Ramchandran</p>
           </div>
 
           <div className="eboard-member">
-            <img src="/path-to-image.jpg" alt="E-Board Member" />
+            <img src={jer} alt="E-Board Member" />
             <p>Social Media Manager</p>
             <p>Jerameel Mendoza</p>
           </div>
